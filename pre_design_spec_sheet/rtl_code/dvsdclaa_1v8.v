d23d9ea478caced3455d872e2dbea6d5c4ffdb13
AMsteel <54321510+AmanVerma-21@users.noreply.github.com>
2021-08-19 15:20:37 +0530

    make_short_PG

--- pre_design_spec_sheet/rtl_code/dvsdclaa_1v8.v
+++ pre_design_spec_sheet/rtl_code/dvsdclaa_1v8.v
@@ -5,14 +5,8 @@ module dvsdclaa_1v8(S,Cout,A,B,Cin);
     wire [3:0]P,G,Car;
     output [3:0]S;
     output Cout;
-    assign P[0]=A[0]^B[0];
-    assign P[1]=A[1]^B[1];
-    assign P[2]=A[2]^B[2];
-    assign P[3]=A[3]^B[3];
-    assign G[0]=A[0]&B[0];
-    assign G[1]=A[1]&B[1];
-    assign G[2]=A[2]&B[2];
-    assign G[3]=A[3]&B[3];
+    assign P = A^B;
+    assign G = A&B;
     
     assign Car[0]= G[0] || (P[0]&Cin);
     assign Car[1]= G[1] || (P[1]&G[0]) || (P[1]&P[0]&Cin); 
